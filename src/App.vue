82dd3daba4873b1704854a8b74132a36ee55bde2
Mia Larsen <54668407+miadp88@users.noreply.github.com>
2023-05-17 01:49:25 +0200

    ProductComponent, Productimg

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import HelloWorld from './components/HeaderComponent.vue'
 import 'bootstrap/js/dist/dropdown'
 import FooterComponent from './components/FooterCompenent.vue'
 import HeroComponent from './components/HeroComponent.vue'
+import ProductComponent from './components/ProductComponent.vue'
 </script>
 
 <template>
@@ -21,6 +22,7 @@ import HeroComponent from './components/HeroComponent.vue'
 
   <main>
     <HeroComponent />
+    <ProductComponent />
   </main>
 
   <footer>
@@ -36,12 +38,16 @@ import HeroComponent from './components/HeroComponent.vue'
 import HeaderComponent from './components/HeaderComponent.vue'
 import FooterCompenent from './components/FooterCompenent.vue'
 import HeroComponent from './components/HeroComponent.vue'
+import ProductComponent from './components/ProductComponent.vue'
+
 
   export default {
     components: {
       HeaderComponent,
       FooterComponent,
       HeroComponent,
+      ProductComponent,
+      
    
     },
     data() {
---
+++ src/components/ProductComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+  <div class="row">
+    <div class="col-lg-4 col-md-12">
+        <img src="../assets/image/Aktiv 1NordboQCLab.png" class="image-responsive">
+    </div>
+
+    <div class="col-lg-4 col-md-6">
+        <img src="../assets/image/Aktiv 2NordboCraftMate.png" class="image-responsive">
+    </div>
+
+    <div class="col-lg-4 col-md-6">
+        <img src="../assets/image/Aktiv 3NordboMimic.png" class="image-responsive">
+    </div>
+  </div>
+</div>
+
+</template>
