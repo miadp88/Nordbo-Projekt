cbfe42b60d0833f1ff1f5073bf3bc8c92dec456e
Mia Larsen <54668407+miadp88@users.noreply.github.com>
2023-05-17 10:29:07 +0200

    Improvments on product Cpmponent in main

--- src/components/ProductComponent.vue
+++ src/components/ProductComponent.vue
@@ -1,16 +1,22 @@
 <template>
     <div class="container proCon">
   <div class="row">
-    <div class="col-lg-4 col-md-12">
+    <div class="col-lg-4 col-md-12 ">
         <img src="../assets/image/Aktiv 1NordboQCLab.png" class="image-responsive">
+        <h2>Mimic Kit</h2>
+        <p>Teach and guide your robot in real-time</p>
     </div>
 
     <div class="col-lg-4 col-md-6">
         <img src="../assets/image/Aktiv 2NordboCraftMate.png" class="image-responsive">
+        <h2>CraftMate Platform</h2>
+        <p>Scan and construct a highly precise 3D<br>model of a surface to be processed</p>
     </div>
 
     <div class="col-lg-4 col-md-6">
         <img src="../assets/image/Aktiv 3NordboMimic.png" class="image-responsive">
+        <h2>QC Platform</h2>
+        <p>Apowerful vision-based software for <br>quality control</p>
     </div>
   </div>
 </div>
