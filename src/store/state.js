cec343f0edc1191157da03da61ab4177edb9076b
Mia Larsen <54668407+miadp88@users.noreply.github.com>
2023-06-21 13:29:41 +0200

    inkludering af korrekt tekst i spørgeskema

--- src/store/state.js
+++ src/store/state.js
@@ -5,7 +5,7 @@ export default{
             {
                 index: 1,
                 name: 'feature',
-                question: 'Which feature do you need?',
+                question: '1. Which feature do you need?',
                 answers: [
                     {
                         answer: 'Sanding', 
@@ -21,33 +21,114 @@ export default{
                         answer: 'Spray painting',
                         value: 'spray painting', 
                         selected: false,
-                    }
+                    },
+                    {
+                        answer: 'Deburring',
+                        value: 'deburring', 
+                        selected: false,
+                    },
+                    {
+                        answer: 'Polishing',
+                        value: 'polishing', 
+                        selected: false,
+                    },
                     
                 ]
             },
             {
                 index: 2,
                 name: 'Test',
-                question: 'Do you need GUI and mimic settings preset for sanding?',
+                question: '2. Which product are you working with',
 
                 answers: [
                     {
                         answer: 'Wood + composite', 
                         value: 'wood/composite',
                         selected: false, 
-                    }
+                    },
+                    {
+                        answer: 'Textile', 
+                        value: 'textile',
+                        selected: false, 
+                    },
+                    {
+                        answer: 'Metal', 
+                        value: 'metal',
+                        selected: false, 
+                    },
+                    {
+                        answer: 'Plastic', 
+                        value: 'plastic',
+                        selected: false, 
+                    },
                 ]
             },
             {
                 index: 3,
                 name: 'Test 333',
-                question: 'Spørgsmål 3',
+                question: '3. Do you need GUI and mimic settings preset for',
                 answers: [
                     {
-                        answer: 'Wood + composite', 
-                        value: 'wood/composite',
+                        answer: 'Yes', 
+                        value: 'yes',
                         selected: false, 
-                    }
+                    },
+                    {
+                        answer: 'No', 
+                        value: 'no',
+                        selected: false, 
+                    },
+                ]
+            },
+            {
+                index: 4,
+                name: 'Test 444',
+                question: '4. Is the product you are working with targeted at',
+                answers: [
+                    {
+                        answer: 'Yes', 
+                        value: 'yes',
+                        selected: false, 
+                    },
+                    {
+                        answer: 'No', 
+                        value: 'no',
+                        selected: false, 
+                    },
+                ]
+            },
+            {
+                index: 5,
+                name: 'Test 555',
+                question: '5. Do you need joystick/system that gives more',
+                answers: [
+                    {
+                        answer: 'Yes', 
+                        value: 'yes',
+                        selected: false, 
+                    },
+                    {
+                        answer: 'No', 
+                        value: 'no',
+                        selected: false, 
+                    },
+                ]
+            },
+            {
+                index: 6,
+                name: 'Test 666',
+                question: '6. Do you need the possiblity to include an',
+                answers: [
+                    {
+                        answer: 'Yes', 
+                        value: 'yes',
+                        selected: false, 
+                    },
+                    {
+                        answer: 'No', 
+                        value: 'no',
+                        selected: false, 
+                    },
                 ]
             },
 
