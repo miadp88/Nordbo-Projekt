31956e962806a2222607f40cafe2cfc3b5926c49
Mia Larsen <54668407+miadp88@users.noreply.github.com>
2023-06-22 09:09:22 +0200

    reactivating Iframe på Aboutsiden

--- src/components/videoComponent.vue
+++ src/components/videoComponent.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="containervideo row text-center mt-5">
+    <div class="containervideo row text-left mt-5">
         <div id="VideoPlayer col-md-3 offset-md-3">
             <h3 class="Titel mb-4 fw-bold">
                 What is the Mimic Kit?
             </h3>
-            <iframe width="420" height="275" src="https://www.youtube.com/embed/smaNAlXfyrg%22%3E"></iframe> 
+            <iframe width="994" height="559" src="https://www.youtube.com/embed/smaNAlXfyrg" title="Mimic: Unboxing of the Mimic Kit | Nordbo Robotics" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
     
 <hr class="hr" />
